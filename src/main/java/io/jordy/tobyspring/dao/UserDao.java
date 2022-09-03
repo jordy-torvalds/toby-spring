653ca65bacfec18594e879058856e5ae811ff8e1
wonsik.ha <wonsik.ha@myrealtrip.com>
2022-09-03 17:08:07 +0900

    init

---
+++ src/main/java/io/jordy/tobyspring/dao/UserDao.java
@@ -0,0 +1,52 @@
+package io.jordy.tobyspring.dao;
+
+import io.jordy.tobyspring.domain.User;
+
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+
+public class UserDao {
+    public User add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection
+                = getConnection("jdbc:mysql://localhost:3306/default",
+                "root",
+                "password");
+
+        PreparedStatement preparedStatement
+                = connection.prepareStatement("insert into user(id, name, password) values(?,?,?)");
+        preparedStatement.setLong(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+    public User get(Long id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection
+                = getConnection("jdbc:mysql://localhost:3306/default",
+                "root",
+                "password");
+
+        PreparedStatement preparedStatement
+                = connection.prepareStatement("select * from user where id = ?");
+        preparedStatement.setLong(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User(resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+}
---
+++ src/test/java/io/jordy/tobyspring/dao/UserDaoTest.java
@@ -0,0 +1,26 @@
+package io.jordy.tobyspring.dao;
+
+import io.jordy.tobyspring.domain.User;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void user_추가() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User user = new User(new Random().nextLong(), "쪼꼬미", "password");
+
+        User addedUser = userDao.add(user);
+
+        User selectedUser = userDao.get(addedUser.getId());
+        assertEquals(addedUser, selectedUser);
+
+
+
+    }
+}
